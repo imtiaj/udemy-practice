a52bdd13d31e2c891f779eb4019e0a00e7baee14
Mohammad Sharif <R041411013@recursion.com.bd>
2020-08-17 12:47:40 +0600

    created api named employee

---
+++ API/API/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using DLL.Models;
+using DLL.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class EmployeeController : ApiBaseController
+    {
+        private readonly IEmployeeInfoRepository _employeeInfoRepository;
+
+        public EmployeeController(IEmployeeInfoRepository employeeInfoRepository)
+        {
+            _employeeInfoRepository = employeeInfoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployee()
+        {
+            return Ok(_employeeInfoRepository.GetAllEmployee());
+        }
+
+        [HttpGet]
+        [Route("{email}")]
+        public IActionResult GetAEmployee(string email)
+        {
+            return Ok(_employeeInfoRepository.GetAEmployee(email));
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeInfo employeeInfo)
+        {
+            return Ok(_employeeInfoRepository.AddEmployeeInfo(employeeInfo));
+        }
+    }
+}
---
+++ API/DLL/Repository/IEmployeeInfoRepository.cs
@@ -0,0 +1,44 @@
+﻿using DLL.DbContext;
+using DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLL.Repository
+{
+    public interface IEmployeeInfoRepository
+    {
+        List<EmployeeInfo> GetAllEmployee();
+        EmployeeInfo GetAEmployee(string email);
+        EmployeeInfo AddEmployeeInfo(EmployeeInfo employeeInfo);        
+    }
+
+    public class EmployeeInfoRepository : IEmployeeInfoRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EmployeeInfoRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public EmployeeInfo AddEmployeeInfo(EmployeeInfo employeeInfo)
+        {
+            _dbContext.EmployeeInfos.Add(employeeInfo);
+            _dbContext.SaveChanges();
+            return employeeInfo;
+        }
+
+        public EmployeeInfo GetAEmployee(string email)
+        {
+            var aEmployee = _dbContext.EmployeeInfos.FirstOrDefault(e => e.Email == email);
+            return aEmployee;
+        }
+
+        public List<EmployeeInfo> GetAllEmployee()
+        {
+            return _dbContext.EmployeeInfos.ToList();
+        }
+    }
+}
