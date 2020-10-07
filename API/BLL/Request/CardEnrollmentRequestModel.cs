5aaf7714c49fbd3e5a706e41f30d975aed5db71a
Asif Newaz <asif.newaz@r-cis.com>
2020-10-07 13:28:38 +0600

    Card Enrolment request model generated

---
+++ API/BLL/Request/CardEnrollmentRequestModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentValidation;
+
+namespace DLL.RequestResponse
+{
+    public class CardEnrollmentRequestModel
+    {
+        public string deviceID { get; set; }
+        public string cvminfo { get; set; }
+        public string pin { get; set; }
+    
+    }
+
+    public class CardEnrollmentRequestModelValidator : AbstractValidator<CardEnrollmentRequestModel>
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public CardEnrollmentRequestModelValidator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            RuleFor(x => x.deviceID).NotNull();
+            RuleFor(x => x.cvminfo).NotNull();
+            RuleFor(x => x.pin).NotNull();
+            
+        }
+
+        //private async Task<bool> TransactionTypeActive(string transactionType, CancellationToken token)
+        //{
+        //    var unitOfWork = _serviceProvider.GetRequiredService<IUnitOfWork>();
+        //    return await unitOfWork.TransactionTypeRepository.GetTransactionTypeStatus(ProjectHelper.TransactionType.BlockbusterMovieTicket.ToString());
+        //}
+    }
+
+}
