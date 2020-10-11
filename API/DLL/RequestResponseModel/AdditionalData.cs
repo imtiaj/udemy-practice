548c42f7d044765910df0e2e094558dada54990a
Asif Newaz <asif.newaz@r-cis.com>
2020-10-11 15:16:27 +0600

    Location Change

---
+++ API/DLL/RequestResponseModel/AdditionalData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DLL.Models
+{
+    public class AdditionalData
+    {
+        //Discount Amount, length: 1-13, Format: AN, M/O/C: M,  Description:
+        public string DiscountAmt { get; set; }
+
+        //Discount Note, length: 1-50, Format: ANS, M/O/C: C,  Description:
+        public string DiscountNote { get; set; }
+    }
+}
--- API/DLL/RequestResponseModel/CardInfo.cs
+++ API/DLL/RequestResponseModel/CardInfo.cs
@@ -1,37 +1,37 @@
 ﻿using System;
 namespace DLL.Models
 {
-    //1.3.12 
+    //API to AppGateway 1.3.12 
     public class CardInfo
     {
         //Primary Account Number, length: 1-40, Format: AN, M/O/C: C,  Description:
         public string Pan { get; set; }
 
         //Masked, length: 1-40, Format: ANS, M/O/C: C,  Description:
-        public string maskedPan { get; set; }
+        public string MaskedPan { get; set; }
 
         //Card Face ID, length: 1-6, Format: AN, M/O/C: C,  Description:
-        public string cardFaceID { get; set; }
+        public string CardFaceID { get; set; }
 
         //Account Number, length: 1-40, Format: AN, M/O/C: C,  Description:
-        public string accountNo { get; set; }
+        public string AccountNo { get; set; }
 
         //Expiry Date, length: 5, Format: ANS(MM/YY), M/O/C: C,  Description:
-        public string expiryDate { get; set; }
+        public string ExpiryDate { get; set; }
 
         //CVN2, length: 3, Format: N, M/O/C: C,  Description:
-        public int cvn2 { get; set; }
+        public int Cvn2 { get; set; }
 
         //Card Product Code, length: 1-4, Format: AN, M/O/C: C,  Description:
-        public string prdNo { get; set; }
+        public string PrdNo { get; set; }
 
         //Card Type, length: 1-50, Format: ANS, M/O/C: C,  Description:  Valid values:- Debit Card,  Credit Card, Prepay Card
-        public string cardType { get; set; }
+        public string CardType { get; set; }
 
         //Card Medium, length: 1-50, Format: ANS, M/O/C: C,  Description:  Valid values:- CHIP,  CHIP_COMPOSITE,  VIRTUAL
-        public string cardMed { get; set; }
+        public string CardMed { get; set; }
 
         //Card State, length: 1-50, Format: ANS, M/O/C: C,  Description: Valid values:- ACTIVE, SUSPENDED, DELETED, LOST
-        public string cardState { get; set; }
+        public string CardState { get; set; }
     }
 }
