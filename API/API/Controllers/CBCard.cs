b91ac81b653c875b55d331fdb9530907725e78ca
Asif Newaz <asif.newaz@r-cis.com>
2020-10-11 17:43:22 +0600

    New Controller added

---
+++ API/API/Controllers/CBCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    public class CBCard : ApiBaseController
+    {
+
+
+        [HttpPost("DebitTransaction")]
+        public async Task<IActionResult> DebitTransaction([FromForm] HoldMovieTicketRequestModel requestModel)
+        {
+            requestModel.User = User;
+            var result = await _movieService.HoldMovieTicket(requestModel);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ API/DLL/RequestResponseModel/UPIDebitTransactionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DLL.RequestResponseModel
+{
+    public class UPIDebitTransactionRequest
+    {
+        // Balance Currency, length: 3, N,  Mandatory, A 3-digit numeric value, as defined by [ISO 4217], that indicates the currency code of the balance.
+        public int MsgInfo { get; set; }
+
+        //Balance Amount, length: 1-13, ANS, Mandatory
+        public string BalAmt { get; set; }
+
+        //Available Amount, length: 1-13, ANS, Mandatory
+        public string AvailableAmt { get; set; }
+
+    }
+}
