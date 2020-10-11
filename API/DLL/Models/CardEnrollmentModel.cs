9b26e5ec03d706c5d0050dcd0f96c4a1b5493aa9
R041411013 <R041411013@DEV-05>
2020-10-11 17:33:49 +0600

    created request response model

--- API/DLL/Models/CardEnrollmentModel.cs
+++ API/DLL/Models/CardEnrollmentModel.cs
@@ -8,37 +8,7 @@ namespace DLL.Models
     public class CardEnrollmentModel
     {
         public MsginfoModel msgInfo { get; set; }
-        public Trxinfo trxInfo { get; set; }
-        public MsgResponse msgResponse { get; set; }
-    }    
-
-    public class Trxinfo
-    {
-        public string deviceID { get; set; }
-        public string userID { get; set; }
-        public string prdNo { get; set; }
-        public Cvminfo cvmInfoObj { get; set; }
-        public string strCvmInfo { get; set; }
-    }
-
-    public class Cvminfo
-    {
-        public string Pan { get; set; }
-        public string accountNo { get; set; }
-        public string expiryDate { get; set; }
-        public string cvn2 { get; set; }
-        public string firstName { get; set; }
-        public string midName { get; set; }
-        public string lastName { get; set; }
-        public string mobileNo { get; set; }
-        public string idType { get; set; }
-        public string idNo { get; set; }
-        public string idCountry { get; set; }
-    }
-
-    public class MsgResponse
-    {
-        public string MyProperty { get; set; }
-        public string responseMsg { get; set; }
-    }
+        public TrxinfoModel trxInfo { get; set; }
+        public MsgResponseModel msgResponse { get; set; }
+    }  
 }
---
+++ API/DLL/RequestResponseModel/AccountInfoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLL.RequestResponseModel
+{
+    public class AccountInfoModel
+    {
+        public string token { get; set; }
+        public string pan { get; set; }
+        public string accountNo { get; set; }
+        public string firstName { get; set; }
+        public string midName { get; set; }
+        public string lastName { get; set; }
+        public string accountStatus { get; set; }
+        public string accountTier { get; set; }
+        public string countryCode { get; set; }
+    }
+}
