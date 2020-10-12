f518504b20e14af8a3e0027d0602f1852a26189e
Asif Newaz <asif.newaz@r-cis.com>
2020-10-12 11:55:40 +0600

    value changed

--- API/DLL/RequestResponseModel/UPIDebitTransactionRequest.cs
+++ API/DLL/RequestResponseModel/UPIDebitTransactionRequest.cs
@@ -3,14 +3,21 @@ namespace DLL.RequestResponseModel
 {
     public class UPIDebitTransactionRequest
     {
-        // Balance Currency, length: 3, N,  Mandatory, A 3-digit numeric value, as defined by [ISO 4217], that indicates the currency code of the balance.
+        // Balance Currency, length: 3, Format: MerchantInfo Obj, Reuest: M, “msgType” value is “DEBIT_TRANSACTION”
         public MerchantInfoModel MsgInfo { get; set; }
 
         //Balance Amount, length: 1-13, ANS, Mandatory
-        public string BalAmt { get; set; }
+        public DebitTransactionInformation trxInfo { get; set; }
 
-        //Available Amount, length: 1-13, ANS, Mandatory
-        public string AvailableAmt { get; set; }
+    }
+
+    public class DebitTransactionInformation
+    {
+        // relTrxMsgID, length: 17-49, Format: AN, Reuest: O, 
+        public String relTrxMsgID { get; set; }
+
+        //Related Transaction Type
+        public string relTrxType { get; set; }
 
     }
 }
