816bfc55cc5e4fc213dc81ffd125b2f61c45ed80
R041411013 <R041411013@DEV-05>
2020-10-13 12:37:19 +0600

    removed cached file

--- API/EXT/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EXT
-{
-    public class Class1
-    {
-    }
-}
--- EmpWebApi/src/app/shared/employee-basic-info.model.ts
+++
@@ -1,9 +0,0 @@
-export class EmployeeBasicInfo {
-    EmployeeID: number;
-    FirstName: string;
-    LastName: string;
-    DateOfBirth: string;
-    Gender: string;
-    NationalID: string;
-    Email: string;
-}
